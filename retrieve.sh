0baf7e8b70feb2927e36f07bf06362cdefea0d83
Ian Agpawa <agpawaji@gmail.com>
2018-03-17 23:55:29 -0400

    feat: Start getRepo function

--- retrieve.sh
+++ retrieve.sh
@@ -1,1 +1,21 @@
 #!/bin/bash
+
+
+getRepos(){
+    filename='repo_names.txt'
+    filelines=$(cat $filename)
+
+    location='/media/onyx/Daedra/archive'
+
+    for line in $filelines
+    do
+        if [ -d "$location/$line" ]; then
+            echo "$location/$line"
+        fi
+
+        # address="git@github.com:ianagpawa/$line"
+
+    done
+}
+
+getRepos
